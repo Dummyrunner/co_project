c139aec3b459c966e5f91e1bac30892633756bb9
Daniel Bergmann <bergmann.da@gmx.de>
2020-06-22 13:07:38 +0200

    adjusted parameters in newtonquadpd(), better ls params chosen

--- code/testfunctions_init.m
+++ code/testfunctions_init.m
@@ -1,9 +1,14 @@
-% fpor NewtonOpt (primal only)
+% Initialization of Example Optimization Problem
+
+
+% for NewtonOpt (primal only)
 % q = [ 1; 3];
 % r = 0.5;
 % f = @(x) x'*P*x + q'*x + r;
 % gradf = @(x) P*x + q;
 % Hessf = @(x) P;
+% arm_beta = 0.5;
+% arm_gamma = 0.01;
 
 Aineq = [0 1]; bineq = 10;
 Aeq = [1 -1]; beq = 0;
@@ -12,8 +17,12 @@ Q = diag([1 2]);
 c = [1;2];
 x0 = [5;5];
 
-arm_beta = 0.5;
-arm_gamma = 0.01;
+%  m,p = 1
+%  n = 2
+
+
+ls_alpha = 0.05;
+ls_beta = 0.5;
 r = [5;5;1;1];
 mu_barrier = 0.5;
-l = newtonquad_pd(Q,c,Aineq, bineq, beq, Aeq, arm_beta, arm_gamma,r,0.9, 1e-5);
+l = newtonquad_pd(Q, c, Aineq, bineq, Aeq, beq, ls_alpha, ls_beta,[1; 1],1,1,0.9);
