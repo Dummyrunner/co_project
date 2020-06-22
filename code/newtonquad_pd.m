cf378aeecad231f595493235f2745a33adf0d9e2
Daniel Bergmann <bergmann.da@gmx.de>
2020-06-22 13:06:21 +0200

    fixed stoppingcr. linesearch; added m as param. to rfromxln

--- code/newtonquad_pd.m
+++ code/newtonquad_pd.m
@@ -1,4 +1,4 @@
-function [x_new, lambda_new, nu_new] = newtonquad_pd(Q, c, Aineq, bineq, beq, Aeq, ls_alpha, ls_beta, x,lambda, mu_barrier)
+function [x_new, lambda_new, nu_new] = newtonquad_pd(Q, c, Aineq, bineq, Aeq, beq, ls_alpha, ls_beta, x,lambda, nu, mu_barrier)
 %UNTITLED Summary of this function goes here
 %   f(x) = 0.5*x'Qx + c'x
 
@@ -13,11 +13,11 @@ p = size(Aeq,1);
 % nu = r((n+m+1):(n+m+p));
 
 % Define Matrices for KKT-equality M_kkt*deltar = b_kkt
- M_kkt =  [Q                        Aineq'                  Aeq';...
-            -diag(lambda)*Aineq     -diag(Aineq*x-beq)      zeros(m,p);
-            Aeq                     zeros(p,m)              zeros(m,p) ];
+M_kkt =  [Q                        Aineq'                  Aeq';...
+    -diag(lambda)*Aineq     -diag(Aeq*x-beq)      zeros(m,p);
+    Aeq                     zeros(p,m)              zeros(m,p) ];
 
-b_kkt = -rfromxln(x,lambda,nu,Q,c,Aineq,bineq,Aeq,beq,mu_barrier);
+b_kkt = -rfromxln(x,lambda,nu,Q,c,Aineq,bineq,Aeq,beq,mu_barrier,m);
 
 % Solve KKT-equality to get search direction
 deltaxln = M_kkt\b_kkt;
@@ -37,27 +37,33 @@ smax = min([1 ; lambdaquot]);
 % Backtracking-Linesearch
 % Typical Parameter choices:
 % alpha in [0.01,0.1]; beta in [0.3,0.8]
-s = smax; %S4 0.99 smax??
+s = 0.99*smax; %S4 0.99 smax??
 found = 0;
 
+
+disp('start linesearch ...');
 while found == 0
     s = s*ls_beta;
+    disp(num2str(s))
+    disp(newline)
     x_new = x + s.*deltax;
     lambda_new = lambda + s.*deltalambda;
     nu_new = nu + s.*deltanu;
-    
-    %TODO compute r
-    if Aineq*x_new < 0 && norm(rfromxln(x_new,lambda_new,nu_new,Q,c,Aineq,Aeq,beq,mu_barrier)) <= (1-ls_alpha*s)*norm(rfromxln(x,lambda,nu,Q,c,Aineq,Aeq,beq,mu_barrier))
+    disp(['Aineq*x_new - bineq= ', num2str(Aineq*x_new-bineq)])
+    if (Aineq*x_new - bineq < 0) && norm(rfromxln(x_new,lambda_new,nu_new,Q,c,Aineq,bineq,Aeq,beq,mu_barrier,m)) <= (1-ls_alpha*s)*norm(rfromxln(x,lambda,nu,Q,c,Aineq,bineq,Aeq,beq,mu_barrier,m))
         found = 1;
     end
+    
+    disp(['found = ',num2str(found)]);
 end
+
 end
 
 
 % -----------------------------------------------------------------------
-function r = rfromxln(x,lambda,nu,Q,c,Aineq,bineq,Aeq,beq,mu_barrier)
-          r = ...
-              [Q*x + c + Aineq'*lambda + Aeq'*nu;...
-              -diag(lambda)*(Aineq*x - bineq) - mu_barrier.*eye(m,1);...
-              Aeq*x - beq];
+function r = rfromxln(x,lambda,nu,Q,c,Aineq,bineq,Aeq,beq,mu_barrier,m)
+r = ...
+    [Q*x + c + Aineq'*lambda + Aeq'*nu;...
+    -diag(lambda)*(Aineq*x - bineq) - mu_barrier.*ones(m,1);...
+    Aeq*x - beq];
 end
