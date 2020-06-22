0de7001a6188f07862db7417d57e6a301ce76328
Daniel Bergmann <bergmann.da@gmx.de>
2020-06-22 18:41:11 +0200

    made testfuncitons script run ipquad_pd.m, comment out unneccessary lines

--- code/testfunctions_init.m
+++ code/testfunctions_init.m
@@ -15,7 +15,7 @@ Aeq = [1 -1]; beq = 0;
 
 Q = diag([1 2]);
 c = [1;2];
-x0 = [5;5];
+% x0 = [5;5];
 
 %  m,p = 1
 %  n = 2
@@ -24,5 +24,11 @@ x0 = [5;5];
 ls_alpha = 0.05;
 ls_beta = 0.5;
 r = [5;5;1;1];
-mu_barrier = 0.5;
-l = newtonquad_pd(Q, c, Aineq, bineq, Aeq, beq, ls_alpha, ls_beta,[1; 1],1,1,0.9);
+% mu_barrier = 0.5;
+% l = newtonquad_pd(Q, c, Aineq, bineq, Aeq, beq, ls_alpha, ls_beta,[1; 1],1,1,0.9);
+
+gamma = .1;
+eps_feas = 1e-4;
+eps_opt = 1e-4;
+
+[x,fval,lambda,nu,eta] = ipquad_pd(Q,c,Aineq,bineq,Aeq,beq,[1; 1],1,1,gamma,eps_feas,eps_opt,ls_alpha,ls_beta);
