0071a3d7858031e1e8bacb2f4726a2894e7de4b3
Daniel Bergmann <bergmann.da@gmx.de>
2020-07-10 20:15:30 +0200

    completed mpc2quad function

--- code/mpcZTC2quadprog.m
+++ code/mpcZTC2quadprog.m
@@ -1,15 +1,36 @@
-function [Q,c,Aineq,Aeq] = mpcZTC2quadprog(Ad,Bd,Q,R,delta,N,xinit)
+function [Q,c,Aineq,bineq,Aeq,beq] = mpcZTC2quadprog(Ad,Bd,Q,R,delta,N,xinit)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 %   reprasing ztc mpc at time t, horizon N samples
 n = size(Ad,1);
 m = size(Bd,2);
 
+% Build Matrix H
 Qs = repmat({Q},N+1,1);
 Rs = repmat({R},N,1);
 H = zeros(N*(n+m)+n);
 Qblk = blkdiag(Qs{:});
 Rblk = blkdiag(Rs{:});
-H = blkdiag(Qblk,zeros(n),Rblk)
+H = delta.*blkdiag(Qblk,zeros(n),Rblk);
+
+xnormbound = 1; %transfer to arguments
+unormbound = 1;
+
+% express box constraints for pred. state and input in inequality constr.
+Aineq = [xnormbound.*eye(n) zeros(n,m);...
+         -xnormbound.*eye(n) zeros(n,m);...
+         zeros(m,n) unormbound.*eye(m);...
+         zeros(m,n) -unormbound.*eye(m)];
+bineq = ones(2*(n+m));
+
+% express system dynamics and init. cond. in equality constr.
+Aeqx = kron(eye(N+1),eye(n));  Aeqx(1:n,1:n) = -1.*Aeqx(1:n,1:n);
+Aeqx = Aeqx + kron(diag(ones(N,1),-1),Ad);
+lastrow = zeros(1,N+1); lastrow(end) = 1;
+Aeqx = [Aeqx; kron(lastrow,eye(n))];
+
+Aequ = kron(eye(N),Bd);
+Aequ = [zeros(N,N*m); Aequ];
+beq = [xinit; zeros(N*m,1)];
 end
 
